d7f82a8e9cdba1723b9d1da19497e0f4d4e4c905
Alex Kopitsa <61422660+Annarex@users.noreply.github.com>
2025-01-27 21:04:55 +0300

    мелочи + создание модели для постов

--- resources/views/components/logo-a-footer.blade.php
+++ resources/views/components/logo-a-footer.blade.php
@@ -1,7 +1,8 @@
 <div class="flex justify-center items-center">
     <a href="{{$href}}" target="_blank"
-    class="block m-1 w transition-all duration-500 bg-white hover:border-indigo-600 hover:ring hover:ring-indigo-300 rounded-xl border-1 border-transparent">
+    
+    class="block transition-all duration-300 {{$logocolor??'bg-white'}} hover:border-indigo-600 hover:ring hover:ring-indigo-300 rounded-xl border-1 border-transparent">
     <img src="{{ url('images/logo/'.$lname) }}" alt="{{$alt}}"
-      class="w-48 h-24 object-scale-down rounded-xl">
+      class="w-40 h-20 object-scale-down rounded-xl">
   </a>
 </div>
